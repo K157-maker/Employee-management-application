65c1925295d7b26bae60ea8d58c3a0862d0653c6
K157-maker <langatkevi460@gmail.com>
2026-03-30 14:32:53 +0300

    javascript code added

---
+++ main.js
@@ -0,0 +1,13 @@
+function viewDetails() {
+  alert("Showing employee details");
+}
+
+function editContact() {
+  alert("Edit feature coming soon!");
+}
+
+function deleteContact() {
+  if (confirm("Are you sure you want to delete?")) {
+    alert("Deleted successfully!");
+  }
+}
